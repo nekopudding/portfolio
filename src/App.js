507551e2f7afe3d523c0bcadf28e5a86e7c93b04
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-07-29 21:06:45 -0700

    dev: set up global css, utility classes, section files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/sections/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <div className='stack'>
+        <p className='link'>facebook</p>
+        <p className='link'>github</p>
+        <p className='link'>discord</p>
+      </div>
+      <div className='copyright faded'>© Dean Yang 2022</div>
+    </>
+  )
+}
+
+export default Footer
