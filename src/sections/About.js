fae5c68f36e9a8fb0ab7301115d0a7328925c51b
nekopudding <deany.y.ca@gmail.com>
2022-08-06 12:33:52 -0700

    dev: complete laptop about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import styles from './About.module.css'
+import { aboutSection } from '../assets/data/section-data'
 
 function About() {
   return (
-    <></>
+    <>
+    <section id={aboutSection.id} className={styles.section}>
+      <div className={`flex ${styles.aboutContainer}`}>
+        <img className={styles.aboutImg} src={aboutSection.image} alt={aboutSection.id} />
+        <div className={` stack ${styles.aboutTextBlock}`}>
+          <h2 className={styles.sectionTitle}>{aboutSection.title}</h2>
+          <div className={styles.sectionText}>
+            {aboutSection.text.map(t => <p className={styles.text}>{t}</p>)}
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={`button-text button-outline`}>Contact Me</button>
+          </div>
+        </div>
+      </div>
+    </section>
+      
+    </>
   )
 }
 
