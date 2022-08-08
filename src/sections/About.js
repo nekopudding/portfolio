c9e425e7f7dd1e1ef32f46d89b358a60946dd7d2
nekopudding <deany.y.ca@gmail.com>
2022-08-07 17:07:11 -0700

    dev: complete laptop skills section

--- src/sections/About.js
+++ src/sections/About.js
@@ -6,15 +6,19 @@ function About() {
   return (
     <>
     <section id={aboutSection.id} className={styles.section}>
-      <div className={`flex ${styles.aboutContainer}`}>
-        <img className={styles.aboutImg} src={aboutSection.image} alt={aboutSection.id} />
-        <div className={` stack ${styles.aboutTextBlock}`}>
+      <div className={`flex ${styles.container}`}>
+        <div className={styles.imgContainer}>
+          <img className={styles.img} src={aboutSection.image} alt={aboutSection.id} />
+        </div>
+        <div className={` stack ${styles.textBlock}`}>
           <h2 className={styles.sectionTitle}>{aboutSection.title}</h2>
           <div className={styles.sectionText}>
             {aboutSection.text.map(t => <p className={styles.text}>{t}</p>)}
           </div>
           <div className={styles.buttonContainer}>
-            <button className={`button-text button-outline`}>Contact Me</button>
+            <button className={`button-text button-outline`}>
+              <a href="#contact">Contact Me</a>
+            </button>
           </div>
         </div>
       </div>
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { skillsSection } from '../assets/data/section-data'
+import styles from './Skills.module.css'
 
 function Skills() {
   return (
-    <></>
+    <>
+      <section id={skillsSection.id} className={styles.section}>
+        <div className={`flex ${styles.container}`}>
+          <div>
+            <h2 className={styles.sectionTitle}>{skillsSection.title}</h2>
+            <div className={styles.sectionText}>
+              {skillsSection.text.map(t => <p>{t}</p>)}
+            </div>
+          </div>
+          <div className={styles.skillbarContainer}>
+            <div className={`stack ${styles.skillbar}`}>
+              <h3>Frontend</h3>
+              <div className={`${styles.bar} ${styles.frontend}`}></div>
+            </div>
+            <div className={`stack ${styles.skillbar}`}>
+              <h3>ReactJS</h3>
+              <div className={`${styles.bar} ${styles.reactJs}`}></div>
+            </div>
+            <div className={`stack ${styles.skillbar}`}>
+              <h3>Backend</h3>
+              <div className={`${styles.bar} ${styles.backend}`}></div>
+            </div>
+            <div className={`stack ${styles.skillbar}`}>
+              <h3>Testing</h3>
+              <div className={`${styles.bar} ${styles.testing}`}></div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
