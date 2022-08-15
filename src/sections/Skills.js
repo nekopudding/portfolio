b6c2f7abfdc16ab5c806a5124ac1125984a9a8b2
nekopudding <deany.y.ca@gmail.com>
2022-08-15 15:17:10 -0700

    dev: complete responsive skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react'
 import { skillsSection } from '../assets/data/section-data'
 import styles from './css/Skills.module.css'
@@ -6,32 +8,40 @@ function Skills() {
   return (
     <>
       <section id={skillsSection.id} className={styles.section}>
-        <div className={`flex ${styles.container}`}>
+        <div className={styles.container}>
           <div>
-            <h2 className={styles.sectionTitle}>{skillsSection.title}</h2>
+            <h2 className={styles.sectionTitle} css={css`
+              @media (max-width: 1080px) {
+                display: none;
+              }
+            `}>{skillsSection.title}</h2>
             <div className={styles.sectionText}>
               {skillsSection.text.map(t => <p key={t}>{t}</p>)}
             </div>
           </div>
           <div className={styles.skillbarContainer}>
-            <div className={`stack ${styles.skillbar}`}>
-              <h3>Frontend</h3>
+            <div className={styles.skillbar}>
+              <h3 className={styles.skillbarText}>Frontend</h3>
               <div className={`${styles.bar} ${styles.frontend}`}></div>
             </div>
-            <div className={`stack ${styles.skillbar}`}>
-              <h3>ReactJS</h3>
+            <div className={styles.skillbar}>
+              <h3 className={styles.skillbarText}>ReactJS</h3>
               <div className={`${styles.bar} ${styles.reactJs}`}></div>
             </div>
-            <div className={`stack ${styles.skillbar}`}>
-              <h3>Backend</h3>
+            <div className={styles.skillbar}>
+              <h3 className={styles.skillbarText}>Backend</h3>
               <div className={`${styles.bar} ${styles.backend}`}></div>
             </div>
-            <div className={`stack ${styles.skillbar}`}>
-              <h3>Testing</h3>
+            <div className={styles.skillbar}>
+              <h3 className={styles.skillbarText}>Testing</h3>
               <div className={`${styles.bar} ${styles.testing}`}></div>
             </div>
-
           </div>
+          <h2 className={styles.sectionTitle} css={css`
+              @media (min-width: 1081px) {
+                display: none;
+              }
+          `}>{skillsSection.title}</h2>
         </div>
       </section>
     </>
