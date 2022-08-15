f4f1807a817843322da31a7f83f30893b596a231
nekopudding <deany.y.ca@gmail.com>
2022-08-14 21:07:58 -0700

    dev: complete contact form using formSubmit

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -24,23 +24,29 @@ function Contact() {
     <>
       <section id="contact" className={styles.section}>
         <h2 className={styles.sectionTitle}>Send Me A Message</h2>
-        <div className={styles.form}>
+        <form className={styles.form} 
+          action='https://formsubmit.co/6dba35be5750caf0540c007955da9c13'
+          method='POST'
+        >
+          <input type="hidden" name="_autoresponse" value="Thank you for contacting me! I will get back to you soon."/>
+          <input type="hidden" name="_subject" value={formData.subject}/>
+
           <input className={styles.input} type="text" placeholder='Name' name='name'
-            value={formData.name} onChange={handleInputChange}
+            value={formData.name} onChange={handleInputChange} required
           />
           <input className={styles.input} type="email" placeholder='Email' name='email'
-            value={formData.email} onChange={handleInputChange}
+            value={formData.email} onChange={handleInputChange} required
           />
           <input className={styles.input} type="text" placeholder='Subject' name='subject'
-            value={formData.subject} onChange={handleInputChange}
+            value={formData.subject} onChange={handleInputChange} required
           />
           <textarea className={styles.input} placeholder='Message' name='message' rows='4'
-            value={formData.message} onChange={handleInputChange}
+            value={formData.message} onChange={handleInputChange} required
           />
           <div className={styles.buttonContainer}>
             <button className={`button-filled ${styles.button}`} type="submit" onClick={submitForm}>Submit</button>
           </div>
-        </div>
+        </form>
       </section>
     </>
   )
