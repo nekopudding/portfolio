b6ab67fe0d38316513cd992482661ec2f74e39c5
Dean Yang <61680928+nekopudding@users.noreply.github.com>
2024-03-16 06:32:41 -0700

    update Footer.js

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
       <div className={`${styles.links} stack`}>
         <a href="https://www.facebook.com/dean.y.ca/" target="_blank" rel="noopener noreferrer">Facebook</a>
         <a href="https://github.com/nekopudding" target="_blank" rel="noopener noreferrer">Github</a>
-        <a href="https://discordapp.com/users/%E7%8C%AB%E3%81%95%E3%82%93#4190" target="_blank" rel="noopener noreferrer">Discord</a>
       </div>
       <div className={styles.copyright}>© Created by Dean 2022</div>
     </section>
